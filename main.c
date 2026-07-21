a95eb73b8a858a6164457ba7850b07ef8969f145
johnpetedoe <johnpetedoe@proton.me>
2026-07-21 22:30:32 +0000

    printing intervals and locations, currently hardcoded, will refactor to decouple data-sources

--- main.c
+++ main.c
@@ -1,7 +1,134 @@
-main (int argc, char **argv) {
-    if (argc >= 2) {
-      
+#include <unistd.h>
+#include <stdlib.h>
+
+int f_strlen (char *s) {
+    int i;
+
+    i = 0;
+    while (s[i]) {
+        i++ ;
     }
-  
+    return (i);
+}
+
+void f_putchar (char c) {
+    write(1, &c, 1);
+}
+
+void f_putstr (char *s) {
+    int i;
+    int n;
+    
+    n = f_strlen(s);
+    i = 0;
+    while (i < n) {
+        f_putchar(s[i]);
+        i++;
+    }
+}
+
+void f_putnbr (long long n) {
+    long long tmp;
+
+    tmp = n;
+    if (tmp < 0) 
+    {
+        tmp = -tmp;
+        f_putchar('-');
+    }
+    if ( tmp >= 10 ) 
+    {
+        f_putnbr(tmp / 10);
+        f_putnbr(tmp % 10);
+    } 
+    else 
+    {
+        f_putchar(tmp + '0');
+    } 
+}
+
+/*
+**
+*/
+
+char *f_itoa (int n) {
+    int tmp;
+    int degree;
+    int power;
+    char *s;
+
+
+    tmp = n;
+    degree = 0; 
+    while (tmp) {
+        tmp /= 10;
+        degree++;
+    }
+    
+    s = (char*)malloc(sizeof(char) * degree + 1);
+    s[degree] = '\0';
+
+    power = 1;
+    tmp = degree;
+    while (tmp) {
+        power *= 10;
+        tmp--;
+    }
+
+    while (degree) {
+        s[degree] = n % power + 48;
+        n /= 10;
+        power /= 10;
+        degree--;
+    }
+    return (s);
+}
+
+void format_time (int t, int interval) {
+    int time;
+
+    time = t / interval;
+    
+    if (time < 10) {
+        f_putchar('0');
+    }
+
+    f_putnbr ( time );
+    f_putstr ( " : ");
+
+    if (t % interval < 10) {
+        f_putchar('0');
+    }
+
+    f_putnbr ( t % interval );
+
+    f_putstr ( f_itoa(t / interval) );
+}
+
+int main (int argc, char **argv) {
+    
+    if (argc < 2) {
+        f_putstr(argv[0]);
+        return (0);
+    }
+    
+    int i;
+    int n;
+    int interval;
+
+    interval = 60;
+    i = 0;
+    n = 12 * interval;
+    
+    while (i < n) {
+        format_time(i, interval);
+        
+        if (((i / interval) <= 8) && ((i / interval) > 6)) {
+            f_putstr("\tAC");
+        }
+        f_putchar('\n');
+        i++;
+    }
+
     return (0);
 }
